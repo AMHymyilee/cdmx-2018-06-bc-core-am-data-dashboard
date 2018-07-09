715ef6b626e4edd982862ce86041956e5a7588f1
Adriana Mora <AMHymyilee@users.noreply.github.com>
2018-07-09 12:29:07 -0500

    hasta aqui sirve bien (respaldando lo que hice)

--- src/data.js
+++ src/data.js
@@ -1,14 +1,17 @@
-const url = "https://raw.githubusercontent.com/AMHymyilee/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
+const url = 'https://raw.githubusercontent.com/AMHymyilee/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json';
 //
 window.getData = () => {
   fetch(url)
     .then(result => result.json())
     .then(result => {
-      const students = computeStudentStats(result);
+      const students = computeStudentsStats(result);
       drawStudents(students);
       drawSearch(students);
-    })
-}
+      printSearchCampus(students);
+      printSearchStats(students);
+      const generation = computeGenerationsStats(result);
+    });
+};
 
 window.addStats = progress => {
   let stats = {};
@@ -16,25 +19,26 @@ window.addStats = progress => {
 
   stats.completedPercentage = progress.porcentajeCompletado;
 
-  if (progress.porcentajeCompletado < 60) { //parametros del progreso de las alumnas
-    stats.status = "bottom";
+  if (progress.porcentajeCompletado <= 60) { // parametros del progreso de las alumnas
+    stats.status = 'bottom';
   }
   if (progress.porcentajeCompletado > 60 && progress.porcentajeCompletado < 90) {
-    stats.status = "medium";
-  } else {
-    stats.status = "high";
+    stats.status = 'medium';
   }
-  stats.topics = topicsArray
+  if (progress.porcentajeCompletado >= 90) {
+    stats.status = 'high';
+  }
+  stats.topics = topicsArray;
 
-  let properties = Object.values(topicsArray); 
+  let properties = Object.values(topicsArray);
   properties.forEach(content => {
     content.completedPercentage = content.duracionDeTema;
-    content.percentageDuration = Math.round((content.duracionTemaCompletado * 100) / content.duracionTema)
-     //formula para el promedio de duracion
+    content.percentageDuration = Math.round((content.duracionTemaCompletado * 100) / content.duracionTema);
+    // formula para el promedio de duracion
 
-    let subTopicsArray = content.subtemas; //sacando subtemas
-    let subProperties = Object.values(subTopicsArray); //propiedades de los subtemas
-    subProperties.forEach(subContent => { 
+    let subTopicsArray = content.subtemas; // sacando subtemas
+    let subProperties = Object.values(subTopicsArray); // propiedades de los subtemas
+    subProperties.forEach(subContent => {
       subContent.type = subContent.tipo;
       subContent.duration = subContent.duracionSubtema;
       if (subContent.completado === 1) {
@@ -46,52 +50,102 @@ window.addStats = progress => {
   });
 
   return stats;
-}
+};
 
-window.computeStudentStats = laboratoria => { //Pasamos argumento result a parámetro laboratoria
-  //console.log(laboratoria);
-  let campus = "";
-  let generationArray = "";
-  let allGenerations = "";
-  let allGenerationsContent = "";
-  let students = "";
+window.computeStudentsStats = laboratoria => { // Pasamos argumento result a parámetro laboratoria
+  // console.log(laboratoria);
+  let campus = '';
+  let generationArray = '';
+  let allGenerations = '';
+  let allGenerationsContent = '';
+  let students = '';
   let studentArray = [];
   let i = 0;
 
-  campus = Object.keys(laboratoria); //traer el nombre de las propiedades
+  campus = Object.keys(laboratoria); // traer el nombre de las propiedades
   generationArray = Object.values(laboratoria);
 
   generationArray.forEach(element => {
     allGenerations = Object.keys(element.generacion);
-    allGenerationsContent = Object.values(element.generacion)
+    allGenerationsContent = Object.values(element.generacion);
+    j = 0;
     allGenerationsContent.forEach(student => {
-        j = 0;
       students = student.estudiantes;
       students.forEach(infoStudent => {
         let stats = addStats(infoStudent.progreso);
         studentArray.push({
-            "campus":campus[i], 
-            "generation":allGenerations[j],
-            "name": infoStudent.nombre,
-            "email": infoStudent.correo,
-            "turn":infoStudent.turno,
-            "stats": stats
-        })
+          'campus': campus[i],
+          'generation': allGenerations[j],
+          'name': infoStudent.nombre,
+          'email': infoStudent.correo,
+          'turn': infoStudent.turno,
+          'stats': stats
+        });
       });
-      j++
-    })
+      j++;
+    });
     i++;
   });
-//console.log(studentArray)
-return studentArray;
-}
+  // console.log(studentArray)
+  return studentArray;
+};
 
- window.filterStudents = (student, search) => {
-     let searchResult = [];
-     student.forEach(clues => {
-        if (clues.name.indexOf(search) !== -1){ //if result exist then push to array
-            searchResult.push(clues);
-        }
-     });
-     return searchResult;
- }
+window.filterCampus = (student, search) => {
+  let searchResult = [];
+  //console.log(student);
+  student.forEach(clues => {
+    if (clues.campus === search) {
+      searchResult.push(clues);
+    }
+  });
+  // console.log(element);
+  return searchResult;
+};
+
+window.filterStats = (student, search) => {
+  let searchResult = [];
+  student.forEach(clues => {
+    //console.log(clues.stats.status);
+    if (clues.stats.status === search) { // if result exist then push to array
+      searchResult.push(clues);
+    }
+  });
+  return searchResult;
+};
+
+window.filterStudents = (student, search) => {
+  let searchResult = [];
+  student.forEach(clues => {
+    if (clues.name.indexOf(search) !== -1) { // if result exist then push to array
+      searchResult.push(clues);
+    }
+  });
+  return searchResult;
+};
+
+
+window.computeGenerationsStats = laboratoria => {
+  /*
+  generationArr = [];
+  let i = 0;
+  let j = 0;
+  let suma = 0;
+  let campus = object.keys(laboratoria);
+  let generation = Object.values(laboratoria);
+  generation.forEach(element => {
+    let years = Object.values(element.generacion);
+    let generations = Object.values(element.generacion);
+    j = 0;
+    years.forEach(students => {
+      let estudiantes = students.estudiantes;
+      estudiantes.forEach(studentInfo => {
+        sum += studentInfo.progreso.porcentajeCompletado;
+      });
+      let average = Math.round(sum / students.estudiantes.length);
+      generationArr.push( );
+    });
+  });*/
+}; 
+
+
+window.sortStudents = (laboratoria) => {};
--- src/main.js
+++ src/main.js
@@ -1,12 +1,165 @@
 const drawStudents = students => {
-   // console.log(students);
-}
+  // console.log(students);
+};
 
 const drawSearch = students => {
-    document.getElementById("search").addEventListener("click",(event) => {
-     event.preventDefault()
-     const name = document.getElementById("name-to-search").value;
-     const searchResult = filterStudents (students, name);    
-     console.log(searchResult);
+  document.getElementById('search').addEventListener('click', (event) => {
+    event.preventDefault();
+    const name = document.getElementById('name-to-search').value;
+    if (name !== '' && name !== ' ') {
+      const searchResult = filterStudents(students, name);
+      // console.log(searchResult);
+      let result = '';
+      if (searchResult.length === 0) {
+        document.getElementById('no-result').innerHTML = '<p> El termino que esta buscando no existe </p>';
+      } else {
+        searchResult.forEach((element, i) => {
+          result += `<tr>
+                    <th scope="row">${i++}</th>
+                    <td>${element.name}</td>
+                    <td>${element.campus}</td>
+                    <td>${element.generation}</td>
+                    <td>${element.email}</td>
+                    <td>${element.stats.completedPercentage}
+                    %</td>
+                    </tr>`;
+        });
+        document.getElementById('no-result').innerHTML = '';
+        document.getElementById('table-body').innerHTML = result;
+      }
     }
- )}
+  });
+};
+
+const printSearchCampus = students => {
+  document.getElementById('campus-Lima').addEventListener('click', (event) => {
+    event.preventDefault();
+    const searchResult = filterCampus(students, 'lima');
+    // Para imprimir en el DOM
+    let result = '';
+    if (searchResult.length === 0) {
+      document.getElementById('no-result').innerHTML = '<p>No existen resultados</p>';
+    } else {
+      searchResult.forEach((element, i) => {
+        result += `<tr>
+                   <th scope="row">${i + 1}</th>
+                   <td>${element.name}</td>
+                    <td>${element.campus}</td>
+                    <td>${element.generation}</td>
+                    <td>${element.email}</td>
+                    <td>${element.stats.completedPercentage}
+                    %</td>
+                 </tr>`;
+      });
+      document.getElementById('no-result').innerHTML = '';
+      document.getElementById('table-body').innerHTML = result;
+    }
+  });
+
+  document.getElementById('campus-Mexico').addEventListener('click', (event) => {
+    event.preventDefault();
+    const searchResult = filterCampus(students, 'mexico');
+    // Para imprimir en el DOM
+    let result = '';
+    if (searchResult.length === 0) {
+      document.getElementById('no-result').innerHTML = '<p>No existen resultados</p>';
+    } else {
+      searchResult.forEach((element, i) => {
+        result += `<tr>
+                   <th scope="row">${i + 1}</th>
+                   <td>${element.name}</td>
+                    <td>${element.campus}</td>
+                    <td>${element.generation}</td>
+                    <td>${element.email}</td>
+                    <td>${element.stats.completedPercentage}
+                    %</td>
+                 </tr>`;
+      });
+      document.getElementById('no-result').innerHTML = '';
+      document.getElementById('table-body').innerHTML = result;
+    }
+  });
+
+  document.getElementById('campus-Santiago').addEventListener('click', (event) => {
+    event.preventDefault();
+    const searchResult = filterCampus(students, 'santiago');
+    // Para imprimir en el DOM
+    let result = '';
+    if (searchResult.length === 0) {
+      document.getElementById('no-result').innerHTML = '<p>No existen resultados</p>';
+    } else {
+      searchResult.forEach((element, i) => {
+        result += `<tr>
+                   <th scope="row">${i + 1}</th>
+                   <td>${element.name}</td>
+                    <td>${element.campus}</td>
+                    <td>${element.generation}</td>
+                    <td>${element.email}</td>
+                    <td>${element.stats.completedPercentage}
+                    %</td>
+                 </tr>`;
+      });
+      document.getElementById('no-result').innerHTML = '';
+      document.getElementById('table-body').innerHTML = result;
+    }
+  });
+};
+
+const printSearchStats = students => {
+  document.getElementById('stats-bottom').addEventListener('click', (event) => {
+    event.preventDefault();
+    const searchResult = filterStats(students, 'bottom');
+    let result = '';
+    searchResult.forEach((element, i) => {
+      result += `<tr>
+                   <th scope="row">${i + 1}</th>
+                   <td>${element.name}</td>
+                    <td>${element.campus}</td>
+                    <td>${element.generation}</td>
+                    <td>${element.email}</td>
+                    <td>${element.stats.completedPercentage}
+                    %</td>
+                 </tr>`;
+    });
+    document.getElementById('no-result').innerHTML = '';
+    document.getElementById('table-body').innerHTML = result;
+  });
+
+  document.getElementById('stats-medium').addEventListener('click', (event) => {
+    event.preventDefault();
+    const searchResult = filterStats(students, 'medium');
+    let result = '';
+    searchResult.forEach((element, i) => {
+      result += `<tr>
+                   <th scope="row">${i + 1}</th>
+                   <td>${element.name}</td>
+                    <td>${element.campus}</td>
+                    <td>${element.generation}</td>
+                    <td>${element.email}</td>
+                    <td>${element.stats.completedPercentage}
+                    %</td>
+                 </tr>`;
+    });
+    document.getElementById('no-result').innerHTML = '';
+    document.getElementById('table-body').innerHTML = result;
+  });
+
+  document.getElementById('stats-high').addEventListener('click', (event) => {
+    event.preventDefault();
+    const searchResult = filterStats(students, 'high');
+    let result = '';
+    searchResult.forEach((element, i) => {
+      result += `<tr>
+                   <th scope="row">${i + 1}</th>
+                   <td>${element.name}</td>
+                    <td>${element.campus}</td>
+                    <td>${element.generation}</td>
+                    <td>${element.email}</td>
+                    <td>${element.stats.completedPercentage}
+                    %</td>
+                 </tr>`;
+    });
+    document.getElementById('no-result').innerHTML = '';
+    document.getElementById('table-body').innerHTML = result;
+  });
+};
